3d58744e8cd521d2dc498f6b5790b288c3309545
MtlJ1991 <MtlJ1991@gmail.com>
2018-02-07 14:10:53 +0000

    spec file set up, api/topics tested

---
+++ spec/test.spec.js
@@ -0,0 +1,40 @@
+const expect = require('chai').expect;
+const seed = require('../seed/test.seed');
+const app = require('../server');
+const request = require('supertest')(app);
+const mongoose = require('mongoose');
+
+describe('API endpoints', () => {
+  let docs = {};
+  beforeEach(function () {
+    return mongoose.connection.dropDatabase()
+      .then(() => {
+        return seed();
+      })
+      .then((data) => {
+        docs = data;
+        return;
+      });
+  });
+  after(() => {
+    mongoose.disconnect();
+  });
+  
+  describe('API endpoint /api/topics', () => {
+    it('GET /topics returns an object of all topics', () => {
+      return request
+        .get('/api/topics')
+        .expect(200)
+        .then((res) => {
+          expect(res.body).to.be.an('object');
+          expect(Object.values(res.body).length).to.be.eql(1);
+          expect(Object.keys(res.body).length).to.be.eql(1);
+          expect(res.body.topic.length).to.be.eql(3);
+          return;
+        });
+    });
+  
+
+  
+  });
+});

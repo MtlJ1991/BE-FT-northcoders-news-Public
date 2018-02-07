6233cfbef8b9453961366429d92d744edd37c3d2
MtlJ1991 <MtlJ1991@gmail.com>
2018-02-07 14:12:30 +0000

    artice endpoint tests added to spec

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -33,6 +33,33 @@ describe('API endpoints', () => {
           return;
         });
     });
+
+
+    it('GET /topics/:topic_id/articles returns an object of all articles related to that topic', () => {
+      return request
+        .get('/api/articles')
+        .expect(200)
+        .then((res) => {
+          expect(res.body).to.be.an('object');
+          expect(Object.values(res.body).length).to.be.eql(1);
+          expect(Object.keys(res.body).length).to.be.eql(1);
+          expect(res.body.topic.length).to.be.eql(2);
+          return;
+        });
+    });
+
+    it('GET /topics/:topic_id/articles returns an object of all articles related to that topic', () => {
+      return request
+        .get('/api/topics/Football/articles')
+        .expect(200)
+        .then((res) => {
+          expect(res.body).to.be.an('object');
+          expect(Object.values(res.body).length).to.be.eql(1);
+          expect(Object.keys(res.body).length).to.be.eql(1);
+          expect(res.body.articles.length).to.be.eql(1);
+          return;
+        });
+    });
   
 
   

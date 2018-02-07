55338fb81914f1f593f5eb073d72387ec332e032
MtlJ1991 <MtlJ1991@gmail.com>
2018-02-07 14:03:02 +0000

    DB seeded, endpoints working as intended

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -3,8 +3,10 @@ const users = require('../models/users');
 const getUser = ((req, res, next) => {
   users.find({username: req.params.username}).lean()
     .then(user => {
-      res.json({ user });
+      console.log(user);
+      return res.json({ user });
     });
+
 });
 
 module.exports = getUser;

ba2c689ae98815201a9907c75f47163c2489e4ef
MtlJ1991 <MtlJ1991@gmail.com>
2018-02-07 12:10:14 +0000

    topics controllers added. ES6 refactor.

---
+++ controllers/topicsControllers.js
@@ -0,0 +1,32 @@
+const topics = require('../models/topics');
+const articles = require('../models/articles');
+
+
+const getAllTopics = ((req, res, next) => {
+  topics.find().lean()
+    .then(topic => {
+      return res.json({
+        topic
+      });
+    });
+});
+
+
+const getArticlesByTopic = ((req, res, next) => {
+
+  topics.find({title: req.params.topic_title})
+
+    .then(topic => {
+      let title = topic[0].title.toLowerCase();
+      return articles.find({belongs_to: title });
+    })
+    .then(articles => {
+      res.json({articles});
+    })
+    .catch(console.error);
+});
+
+module.exports = {
+  getAllTopics,
+  getArticlesByTopic
+};

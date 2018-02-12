a701272a01214c33c2eed62ae8de8995b58ed292
MtlJ1991 <MtlJ1991@gmail.com>
2018-02-12 15:18:47 +0000

    changes to comment votes

--- controllers/commentsControllers.js
+++ controllers/commentsControllers.js
@@ -4,16 +4,16 @@ const comments = require('../models/comments');
 const changeNumOfVotes = ((req, res, next) => {
   if ((req.query.vote !== 'up') && (req.query.vote !== 'down')) next({ message: 'Invalid vote command, please vote up or down.', status: 400 });
 
-  return comments.findByIdAndUpdate(req.params.comment_id).lean()
+  return comments.findByIdAndUpdate(req.params.comment_id)
     .then(comment => {
       if(req.query.vote === 'up') comment.votes ++;
       else if(req.query.vote === 'down') comment.votes --;
-      res.json({comment});
-    }).catch(next);
-    
-    
+      return comment.save();
+    }).then(comment => res.status(200).send({comment}))
+    .catch(next);
 });
 
+    
 const deleteComment = ((req, res, next) => {
   if (req.params.comment_id.length !== 24) next({ error: 'Invalid comment, can\'t remove', status: 400});
 

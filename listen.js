4b5643e8a3f1b9b68d34ea10d43a0acfcfe5a9bc
MtlJ1991 <MtlJ1991@gmail.com>
2018-02-12 12:09:37 +0000

    changes to listen.js, PORT

--- listen.js
+++ listen.js
@@ -2,9 +2,9 @@ const app = require('./server');
 
 
 if (!process.env.NODE_ENV) process.env.NODE_ENV = 'dev';
+const PORT = require('./config').PORT[process.env.NODE_ENV];
 let config = require('./config');
 let db = process.env.DB || config.DB[process.env.NODE_ENV];
-
 
 app.listen(PORT, function () {
   console.log(`listening on port ${PORT}`);
